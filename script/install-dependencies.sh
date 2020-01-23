4981416bda4f71a286ab0532d5616d2be3354880
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-01-23 11:04:59 -0800

    chore: add `Makefile` to ease building

---
+++ script/install-dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SUDO=sudo
+
+if [ "$(id --user)" = 0 ]; then
+  SUDO=
+fi
+
+# Install native package build dependencies.
+$SUDO apt-get install -y build-essential libudev-dev unzip git python
+
+if ! command -v node >/dev/null 2>&1; then
+  echo "node not found. please install it before continuing: https://nodejs.org/en/" >&2
+  exit 127
+fi
+
+if ! command -v yarn >/dev/null 2>&1; then
+  echo "yarn not found. please install it before continuing: https://www.yarnpkg.com/en/" >&2
+  exit 127
+fi
+
+# Install kiosk-browser dependencies.
+yarn install
