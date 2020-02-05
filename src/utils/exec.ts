27f2c74226da227d2a2967e3c5cba1d3efec5178
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-02-04 17:32:04 -0800

    refactor(security): print by piping to `lpr` instead of temp file
    If we never write a temp file to disk we never run the risk of that data being preserved longer than we want it to be.

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,8 +1,63 @@
-import { execFile } from 'child_process'
-import { promisify } from 'util'
+import { spawn } from 'child_process'
+import makeDebug from 'debug'
 
-// This is here mainly to make mocking easier.
-export default promisify(execFile)
+const debug = makeDebug('kiosk-browser:exec')
+
+/**
+ * Like `child_process.exec`, but with easy stdin.
+ */
+export default async function exec(
+  file: string,
+  args: readonly string[] = [],
+  stdin?: string | Buffer,
+): Promise<{ stdout: string; stderr: string }> {
+  const child = spawn(file, args)
+  let stdout = ''
+  let stderr = ''
+
+  debug(
+    'running command=%s args=%o stdin=%s pid=%d',
+    file,
+    args,
+    typeof stdin,
+    child.pid,
+  )
+
+  child.stdout.on('data', chunk => {
+    stdout += chunk
+  })
+
+  child.stderr.on('data', chunk => {
+    stderr += chunk
+  })
+
+  return new Promise((resolve, reject) => {
+    child.on('exit', (code, signal) => {
+      debug(
+        'process %d exited with code=%d, signal=%s (command=%s args=%o)',
+        child.pid,
+        code,
+        signal,
+        file,
+        args,
+      )
+
+      if (code === 0) {
+        resolve({ stdout, stderr })
+      } else {
+        reject(
+          makeExecError({
+            code: code ?? undefined,
+            signal,
+            stdout,
+            stderr,
+            cmd: file,
+          }),
+        )
+      }
+    })
+  })
+}
 
 export interface ExecError {
   code: number
