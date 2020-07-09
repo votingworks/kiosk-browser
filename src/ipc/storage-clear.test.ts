f3e0d217d98f4b9ef347334ff5cb6b87c7e0653f
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-07-08 20:52:02 -0700

    refactor: minor improvements to storage code
    - use method syntax for storage methods
    - remove `undefined` hack
    - ensure falsy values can be returned from `get`
    - consolidate mocks for electron-json-storage into a single file

--- src/ipc/storage-clear.test.ts
+++ src/ipc/storage-clear.test.ts
@@ -1,23 +1,21 @@
 import { fakeIpc } from '../../test/ipc'
 import register, { channel as storageClearChannel } from './storage-clear'
-
 import storage from 'electron-json-storage'
+import { promisify } from 'util'
 
-type Callback = (error: string) => void
-
-jest.mock('electron-json-storage', () => ({
-  __esModule: true,
-  default: {
-    clear: jest.fn((callback: Callback) => callback('')),
-  },
-}))
+const get = promisify(storage.get)
+const set = promisify(storage.set)
 
 test('store works', async () => {
   const { ipcMain, ipcRenderer } = fakeIpc()
 
   register(ipcMain)
 
+  await set('a', { a: 1 })
+  await set('b', { b: 2 })
   await ipcRenderer.invoke(storageClearChannel)
 
   expect(storage.clear).toHaveBeenCalledWith(expect.any(Function))
+  expect(await get('a')).toEqual(undefined)
+  expect(await get('b')).toEqual(undefined)
 })
