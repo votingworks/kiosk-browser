60ceb3eb6e367911c6490bf1ad9daa9b2bb53519
Ben Adida <ben@adida.net>
2021-09-15 17:53:22 -0400

    add ability to sign with signify and a secret key designated at startup time
    Co-authored-by: Brian Donovan <1938+eventualbuddha@users.noreply.github.com>

---
+++ src/ipc/sign.test.ts
@@ -0,0 +1,122 @@
+import { Subject } from 'rxjs'
+import { Device } from 'usb-detection'
+import { fakeIpc } from '../../test/ipc'
+import mockOf from '../../test/mockOf'
+import exec from '../utils/exec'
+import register, { channel as signChannel } from './sign'
+
+const execMock = mockOf(exec)
+jest.mock('../utils/exec')
+
+beforeEach(() => {
+  execMock.mockReset()
+  execMock.mockResolvedValue({ stdout: '', stderr: '' })
+})
+
+const changedDevices = new Subject<Iterable<Device>>()
+const autoconfiguredPrinter = new Subject<void>()
+
+test('call to sign invokes the right signify command, but only if signatureType is well-formed', async () => {
+  const { ipcMain, ipcRenderer } = fakeIpc()
+  register(ipcMain, {
+    changedDevices,
+    autoconfiguredPrinter,
+    options: {
+      signifySecretKey: '/tmp/key.sec',
+      url: new URL('about:blank'),
+      originFilePermissions: [],
+    },
+  })
+
+  execMock.mockResolvedValue({
+    stderr: '',
+    stdout: 'untrusted comment: hello\nFAKESIGNATURERIGHTHERE==\n',
+  })
+
+  const signResult = await ipcRenderer.invoke(signChannel, {
+    signatureType: 'test',
+    payload: 'hello',
+  })
+
+  expect(execMock).toHaveBeenNthCalledWith(
+    1,
+    'signify-openbsd',
+    ['-S', '-s', '/tmp/key.sec', '-m', '-', '-x', '-'],
+    'test.hello',
+  )
+
+  expect(signResult).toBe('FAKESIGNATURERIGHTHERE==')
+
+  execMock.mockReset()
+
+  const badTypeSignResult = await ipcRenderer.invoke(signChannel, {
+    signatureType: 'this.hasperiodsandthatisbad',
+    payload: 'hello',
+  })
+
+  expect(execMock).not.toHaveBeenCalled()
+
+  expect(badTypeSignResult).toBeUndefined()
+})
+
+test('call to sign when no key is registered returns undefined', async () => {
+  const { ipcMain, ipcRenderer } = fakeIpc()
+  register(ipcMain, {
+    changedDevices,
+    autoconfiguredPrinter,
+    options: {
+      url: new URL('about:blank'),
+      originFilePermissions: [],
+    },
+  })
+
+  const signResult = await ipcRenderer.invoke(signChannel, {
+    signatureType: 'test',
+    payload: 'hello',
+  })
+
+  expect(execMock).not.toHaveBeenCalled()
+
+  expect(signResult).toBeUndefined()
+})
+
+test('call to sign when error occurs or exception thrown returns undefined', async () => {
+  const { ipcMain, ipcRenderer } = fakeIpc()
+  register(ipcMain, {
+    changedDevices,
+    autoconfiguredPrinter,
+    options: {
+      signifySecretKey: '/tmp/key.sec',
+      url: new URL('about:blank'),
+      originFilePermissions: [],
+    },
+  })
+
+  execMock.mockResolvedValue({
+    stderr: 'oopsie daisy',
+    stdout:
+      'untrusted comment: hello\nFAKESIGNATURERIGHTHERETHATSHOULDNOTBERETURNEDBECAUSESTDERR==\n',
+  })
+
+  const signResult = await ipcRenderer.invoke(signChannel, {
+    signatureType: 'test',
+    payload: 'hello',
+  })
+
+  expect(execMock).toHaveBeenCalled()
+
+  expect(signResult).toBeUndefined()
+
+  execMock.mockReset()
+
+  execMock.mockRejectedValueOnce('throwing cause I feel like it')
+
+  const signResultWithThrow = await ipcRenderer.invoke(signChannel, {
+    signatureType: 'test',
+    payload: 'hello',
+  })
+
+  expect(execMock).toHaveBeenCalled()
+
+  expect(signResultWithThrow).toBeUndefined()
+})
--- src/utils/options.ts
+++ src/utils/options.ts
@@ -17,6 +17,7 @@ export interface Options {
   autoconfigurePrintConfig?: PrintConfig
   allowDevtools?: boolean
   originFilePermissions: OriginFilePermission[]
+  signifySecretKey?: string
 }
 
 export interface Help {
@@ -38,6 +39,7 @@ async function parseOptionsWithoutTryCatch(
   let helpArg: string | undefined
   let allowDevtoolsArg: boolean | undefined
   let originFilePermissions: OriginFilePermission[] | undefined
+  let signifySecretKey: string | undefined
   const warnings: string[] = []
 
   for (let i = 0; i < argv.length; i++) {
@@ -65,6 +67,14 @@ async function parseOptionsWithoutTryCatch(
         ...(originFilePermissions ?? []),
         parseOriginFilePermissionString(value),
       ]
+    } else if (arg === '--signify-secret-key') {
+      i++
+      const value = argv[i]
+      debug('got option for %s: %s', arg, value)
+      if (!value || value.startsWith('-')) {
+        return { error: new Error(`expected value for option: ${arg}`) }
+      }
+      signifySecretKey = value
     } else if (arg === '--help' || arg === '-h') {
       helpArg = arg
     } else if (!arg.startsWith('-')) {
@@ -106,6 +116,7 @@ async function parseOptionsWithoutTryCatch(
         parseOriginFilePermissionString,
       ) ??
       [],
+    signifySecretKey: signifySecretKey ?? env.KIOSK_BROWSER_SIGNIFY_SECRET_KEY,
   }
 
   debug('parsed options: %O', options)
