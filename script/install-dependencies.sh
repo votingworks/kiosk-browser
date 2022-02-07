afe8bb8ae10e8dda4d4d60b072918294a1e1f16e
Matt Bernhard <mdb92nc@gmail.com>
2022-02-07 15:50:13 -0500

    Forgot I wasn't in a Makefile

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SUDO=sudo
-OS := $(shell lsb_release -cs)
+OS=$(lsb_release -cs)
 
 if [ "$(id --user)" = 0 ]; then
   SUDO=
@@ -12,9 +12,9 @@ fi
 # Install native package build dependencies.
 $SUDO apt-get install -y build-essential libudev-dev unzip git python pmount cups-client
 
-ifeq ($(OS),bullseye)
+if [[ $OS == "bullseye" ]] then;
 	sudo apt install -y libgtk-3-0  libnotify4 libxssl libxtst6 xdg-utils libatspi2.0-0 kde-cli-tools kde-runtime trash-cli libglib2.0-bin gvfs-bin
-endif
+fi
 
 if ! command -v node >/dev/null 2>&1; then
   echo "node not found. please install it before continuing: https://nodejs.org/en/" >&2
