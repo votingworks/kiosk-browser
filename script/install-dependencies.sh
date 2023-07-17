740b6672f673fed152e799e1df130eed8260c672
Adam McManus <adam.mcmanus@gmail.com>
2023-07-17 16:02:44 -0500

    remove apt installs from project

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -10,20 +10,6 @@ if [ "$(id --user)" = 0 ]; then
   SUDO=
 fi
 
-# Install native package build dependencies.
-$SUDO apt-get install -y \
-  build-essential \
-  libudev-dev \
-  unzip \
-  git \
-  python \
-  cups-client \
-  ruby
-
-if [[ $OS == "bullseye" ]]; then
-	$SUDO apt install -y libgtk-3-0  libnotify4 libxss1 libxtst6 xdg-utils libatspi2.0-0 kde-cli-tools trash-cli libglib2.0-bin gvfs-bin cups cups-bsd
-fi
-
 $SUDO gem install fpm
 
 "${DIR}/verify-dependencies.sh"
