42c866f0b6e084c5458393a717cf4bbbcc853ba8
Adam McManus <adam.mcmanus@gmail.com>
2023-07-19 10:05:31 -0500

    dont rely on tilde

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+local_user=`logname`
+local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
 
 "${DIR}/verify-dependencies.sh"
 
@@ -10,8 +12,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 yarn --offline tsc
 
 # Get the electron cache dir
-electron_timestamp=`ls ~/.cache/electron/`
-electron_cache="~/.cache/electron/${electron_timestamp}"
+electron_timestamp=`ls ${local_user_home_dir}/.cache/electron/`
+electron_cache="${local_user_home_dir}/.cache/electron/${electron_timestamp}"
 
 # Build .deb file.
 ELECTRON_CACHE=${electron_cache} yarn --offline app:dist
