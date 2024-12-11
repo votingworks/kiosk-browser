9b9b2437bae6d5fabc7d38207aea08fc6715d159
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-11 06:04:32 -0800

    refactor(preload): strip explicit types

--- src/preload.ts
+++ src/preload.ts
@@ -1,42 +1,38 @@
 import makeDebug from 'debug';
-import {
-  contextBridge,
-  ipcRenderer,
-  OpenDialogOptions,
-  OpenDialogReturnValue,
-} from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 import { channel as showOpenDialogChannel } from './ipc/show-open-dialog';
 import { channel as logChannel } from './ipc/log';
 import { channel as quitChannel } from './ipc/quit';
 import { channel as captureScreenshotChannel } from './ipc/capture-screenshot';
 
 const debug = makeDebug('kiosk-browser:client');
+type Kiosk = KioskBrowser.Kiosk;
 
-function makeKiosk(): KioskBrowser.Kiosk {
+function makeKiosk(): Kiosk {
   return {
-    async showOpenDialog(
-      options?: OpenDialogOptions,
-    ): Promise<OpenDialogReturnValue> {
+    async showOpenDialog(options) {
       debug('forwarding `showOpenDialog` to main process');
       return (await ipcRenderer.invoke(
         showOpenDialogChannel,
         options,
-      )) as OpenDialogReturnValue;
+      )) as ReturnType<Kiosk['showOpenDialog']>;
     },
 
-    async log(message: string): Promise<void> {
+    async log(message) {
       debug('forwarding `log` to the main process');
       await ipcRenderer.invoke(logChannel, message);
     },
 
-    quit(): void {
+    quit() {
       debug('forwarding `quit` to main process');
       void ipcRenderer.invoke(quitChannel);
     },
 
-    async captureScreenshot(): Promise<Buffer> {
+    async captureScreenshot() {
       debug('forwarding `captureScreenshot` to the main process');
-      return (await ipcRenderer.invoke(captureScreenshotChannel)) as Buffer;
+      return (await ipcRenderer.invoke(captureScreenshotChannel)) as ReturnType<
+        Kiosk['captureScreenshot']
+      >;
     },
   };
 }
