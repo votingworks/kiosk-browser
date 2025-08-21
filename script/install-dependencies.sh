f73a73622ac61e34df1d22315110c1fd89402d25
Adam McManus <adam.mcmanus@gmail.com>
2025-08-21 12:40:23 -0500

    testing

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -12,4 +12,4 @@ yarn install --frozen-lockfile
 version=$( grep version package.json | cut -d':' -f2 | xargs | sed 's/,//' )
 prebuilt_package_url="https://votingworks-apt-snapshots.s3.us-west-2.amazonaws.com/kiosk-browser_${version}_amd64.deb"
 
-curl --output-dir dist -O ${prebuilt_package_url} 
+curl --output-dir dist -O ${prebuilt_package_url} || { echo "Couldn't download file, will build from source" }
