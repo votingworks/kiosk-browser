ad0fdab5fc56bc6c7f2235ae4b5f5fb08b4fda72
Adam McManus <adam.mcmanus@gmail.com>
2025-08-21 14:07:19 -0500

    ensure dist directory exists

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -12,4 +12,6 @@ yarn install --frozen-lockfile
 version=$( grep version package.json | cut -d':' -f2 | xargs | sed 's/,//' )
 prebuilt_package_url="https://votingworks-apt-snapshots.s3.us-west-2.amazonaws.com/kiosk-browser_${version}_amd64.deb"
 
+mkdir -p dist
+
 curl -f --output-dir dist -O ${prebuilt_package_url} || echo "Couldn't download file, will build from source"
