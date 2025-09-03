e7d1811ccf67c05601e6f648a553b981c9ab9119
Adam McManus <adam.mcmanus@gmail.com>
2025-09-03 12:48:17 -0500

    Use a prebuilt version of kiosk-browser when available (#182)
    * only build if a prebuilt version is not available
    
    * use prebuilt when available
    
    * version
    
    * dir fix
    
    * testing
    
    * fail on download errors
    
    * typo fix
    
    * ensure dist directory exists
    
    * move into a subfolder for easier s3 management
    
    * add a pull request template that includes a reminder to bump the version in package.json

--- script/build.sh
+++ script/build.sh
@@ -6,14 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 local_user=`logname`
 local_user_home_dir=$( getent passwd "${local_user}" | cut -d: -f6 )
 
-"${DIR}/verify-dependencies.sh"
+version=$( grep version package.json | cut -d':' -f2 | xargs | sed 's/,//' )
+prebuilt_package_path="dist/kiosk-browser_${version}_amd64.deb"
 
-# Build the TypeScript files.
-yarn --offline tsc
+if [[ ! -f ${prebuilt_package_path} ]]; then
+  "${DIR}/verify-dependencies.sh"
 
-# Get the electron cache dir
-electron_timestamp=`ls ${local_user_home_dir}/.cache/electron/`
-electron_cache="${local_user_home_dir}/.cache/electron/${electron_timestamp}"
+  # Build the TypeScript files.
+  yarn --offline tsc
 
-# Build .deb file.
-ELECTRON_SKIP_BINARY_DOWNLOAD=1 ELECTRON_CACHE=${electron_cache} yarn --offline app:dist
+  # Get the electron cache dir
+  electron_timestamp=`ls ${local_user_home_dir}/.cache/electron/`
+  electron_cache="${local_user_home_dir}/.cache/electron/${electron_timestamp}"
+
+  # Build .deb file.
+  ELECTRON_SKIP_BINARY_DOWNLOAD=1 ELECTRON_CACHE=${electron_cache} yarn --offline app:dist
+
+fi
